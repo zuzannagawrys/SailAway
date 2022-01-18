4eea5d4da7ca1b7b2e998c7be222b9bd032e7059
zuzannagawrys <zuza20122000@gmail.com>
2022-01-18 09:52:32 +0100

    map_view and cruise description

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'DefaultController');
+
+Router::run($path);
